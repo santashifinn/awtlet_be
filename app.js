179b7590654853bbabda19e144f00dd537f82cb8
Pip Murphy <santashi.finn@gmail.com>
2026-02-01 20:38:58 +0000

    added error handling

--- app.js
+++ app.js
@@ -4,4 +4,24 @@ const app = express();
 
 app.use(cors());
 
+const apiRouter = require("./routes/api-router");
+const {
+  generalErrorHandler,
+  postgresErrorHandler,
+  customErrorHandler,
+  serverErrorHandler,
+} = require("./errors");
+
+app.use(express.json());
+
+app.use("/api", apiRouter);
+
+app.use(postgresErrorHandler);
+
+app.use(customErrorHandler);
+
+app.use(serverErrorHandler);
+
+app.all("/{*any}", generalErrorHandler);
+
 module.exports = app;
